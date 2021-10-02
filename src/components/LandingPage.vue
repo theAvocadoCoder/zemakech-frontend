5e35836f3615b036bc7b98c2c0ff852be0a71346
Kelechi Nwauwa <nwauwa.kelechi.e@gmail.com>
2021-10-02 23:15:51 +0100

    new commit

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>Zemakech Cuisine</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -104,10 +104,7 @@
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
+  name: "LandingPage",
 };
 </script>
 
